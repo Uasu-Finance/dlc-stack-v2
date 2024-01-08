51cc2a0345ce38178fc8e44bf69be7d28ae22625
Robert Tera <37669800+scolear@users.noreply.github.com>
2024-01-08 13:28:24 +0100

    Fix/attestor dockerfile (#127)
    * fix: dockerfile

--- attestor/observer/src/chains/stacks/get-observer.ts
+++ attestor/observer/src/chains/stacks/get-observer.ts
@@ -4,6 +4,8 @@ import { getConfig } from './config/get-config.js';
 import { Observer } from '../shared/models/observer.interface.js';
 import { ContractConfig, NetworkConfig } from './models/interfaces.js';
 import { fetchTXInfo } from './utilities/api-calls.js';
+import { AddressTransactionWithTransfers } from '@stacks/blockchain-api-client';
+import { Transaction } from '@stacks/stacks-blockchain-api-types';
 
 export default async (config: ChainConfig): Promise<Observer> => {
   let dlcManager: ContractConfig;
@@ -18,29 +20,32 @@ export default async (config: ChainConfig): Promise<Observer> => {
 
   return {
     start: () => {
-      networkConfig.socket.socket.on('address-transaction', async (address, txWithTransfers) => {
-        try {
-          const tx = txWithTransfers.tx;
-          if (tx.tx_status !== 'success') {
-            console.log(`[Stacks] Skip - Failed tx: ${tx.tx_id}`);
-            return;
+      networkConfig.socket.socket.on(
+        'address-transaction',
+        async (address: string, txWithTransfers: AddressTransactionWithTransfers) => {
+          try {
+            const tx = txWithTransfers.tx as Transaction;
+            if (tx.tx_status !== 'success') {
+              console.log(`[Stacks] Skip - Failed tx: ${tx.tx_id}`);
+              return;
+            }
+            if (tx.is_unanchored) {
+              console.log(`[Stacks] Skip - Microblock tx: ${tx.tx_id}`);
+              return;
+            }
+            const txInfo = await fetchTXInfo(tx.tx_id, networkConfig.deploymentInfo.api_base_extended);
+            if (txInfo.event_count < 1) {
+              console.log(`[Stacks] Skip - Non-printing tx: ${tx.tx_id}`);
+              return;
+            }
+            if (dlcManager.checkAddresses(address)) {
+              await dlcManager.handleTx(txInfo);
+            }
+          } catch (error) {
+            console.error(error);
           }
-          if (tx.is_unanchored) {
-            console.log(`[Stacks] Skip - Microblock tx: ${tx.tx_id}`);
-            return;
-          }
-          const txInfo = await fetchTXInfo(tx.tx_id, networkConfig.deploymentInfo.api_base_extended);
-          if (txInfo.event_count < 1) {
-            console.log(`[Stacks] Skip - Non-printing tx: ${tx.tx_id}`);
-            return;
-          }
-          if (dlcManager.checkAddresses(address)) {
-            await dlcManager.handleTx(txInfo);
-          }
-        } catch (error) {
-          console.error(error);
         }
-      });
+      );
     },
   };
 };
